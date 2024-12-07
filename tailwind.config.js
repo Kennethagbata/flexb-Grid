071571dabea9ade64b4975c6c8470d9e09048110
Kennethagbata <agbatakenneth5@gmail.com>
2024-12-07 17:20:00 +0100

    add all changes and updates to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#0a0b1d',
+        'dark': '#1a1939',
+        'bar': '#321a3e',
+        'par': '#9c62ca',
+      }
+    },
   },
   plugins: [],
 }
-
